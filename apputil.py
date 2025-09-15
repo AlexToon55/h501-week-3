4964ea42698d4205672346d3b35606a157f53896
Alex Toon <altoon@IU.EDU>
2025-09-15 14:11:56 -0400

    Exercise 2 submisison
    Exercise 2 submisison

--- apputil.py
+++ apputil.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 
 #excersie 1
+# A simple function to compute the nth Fibonacci number
 def fib(n):
     "Return the nth Fibonacci number."
     if n <= 0:
@@ -19,13 +20,29 @@ def fib(n):
         for _ in range(2, n):
             a, b = b, a + b
         return b
-# A simple function to compute the nth Fibonacci number
+# Example usage
 n = 9  # Example input
 print(fib(n))  # print the answer
 
 
 
 #excersie 2
+# A function to convert a positive integer to its binary representation
+def to_binary(n):
+    "Convert an integer to its binary representation as a string."
+    if n < 0:
+        return "Input should be a non-negative integer."
+    elif n == 0:
+        return "0"
+    bits = []
+    # Convert to binary
+    while n:
+        bits.append(str(n % 2))
+        n //= 2
+    return ''.join(reversed(bits))
+# Example usage
+num = 18  # Example input
+print(to_binary(num))  # print the answer
 
 
 
