6aa4e4c8657e2e8f240f161362376ae7cc8fa9ad
Alex Toon <altoon@IU.EDU>
2025-09-16 09:44:41 -0400

    Exercise 3 task 1 fix
    adding in logic to turn incorrect fields to NA

--- apputil.py
+++ apputil.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 # update/add code below ...
 
-
+print ("Exercise 1 starting")
 #exercise 1
 # A simple function to compute the nth Fibonacci number
 def fibonacci(n):
@@ -43,7 +43,7 @@ n = 9  # Example input
 print(fibonacci(n))  # print the answer
 
 
-
+print ("Exercise 2 starting")
 #exercise 2
 # A function to convert a positive integer to its binary representation
 def to_binary(n):
@@ -64,7 +64,7 @@ print(to_binary(num))  # print the answer
 
 
 
-
+print ("Exercise 3 starting")
 #exercise 3
 
 url = 'https://github.com/melaniewalsh/Intro-Cultural-Analytics/raw/master/book/data/bellevue_almshouse_modified.csv'
@@ -75,18 +75,31 @@ print (df_bellevue.head(40))
 # list all values in the gender column
 print (df_bellevue.gender.unique())
 
+print ("Task 1 starting")
 def task_1(): 
     #Return a list of all column names, *sorted* such that the first column has the *least* missing values, and the last column has the *most* missing values (use the raw column names)
     # *Note: there is an issue with the `gender` column you'll need to remedy first ...*
+
+
+    incorrect_counts = df_bellevue.isin(['H', 'G']).sum()
+    print (incorrect_counts)
+    df_bellevue.replace({'H': pd.NA, 'G': pd.NA}, inplace=True)
+
     missing_counts = df_bellevue.isnull().sum()
     print (missing_counts)
+
     sorted_columns = missing_counts.sort_values().index.tolist()
+    
     print (sorted_columns)
+
+
+
+
     return sorted_columns
 print (task_1())
 
 
-print ("exercise 2 starting")
+print ("Task 2 starting")
 def task_2(): 
     #Return a **data frame** with two columns:
    #- the year (for each year in the data), `year`
@@ -97,9 +110,7 @@ def task_2():
     return admissions_per_year
 print (task_2())
 
-
-
-print ("exercise 3 starting")
+print ("Task 3 starting")
 def task_3(): 
     #Return a **series** with:
     # - Index: gender (for each gender in the data)
@@ -110,7 +121,7 @@ def task_3():
 print (task_3())
 
 
-print ("exercise 4 starting")
+print ("Task 4 starting")
 def task_4(): 
     #Return a list of the 5 most common professions *in order of prevalence* (so, the most common is first).
     top_5_profession = df_bellevue['profession'].value_counts().head(5).index.tolist()
